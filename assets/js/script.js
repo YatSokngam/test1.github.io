6007269179839a3673846cd0d338416fd997bf4d
Yat Sokngam <itknowledge321@gmail.com>
2022-06-04 15:33:17 +0700

    test1

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$("#btn-login").click(function(){
+    $(".main-bg-form-login").show(200);
+});
+    $("#close-login").click(function(){
+        $(".main-bg-form-login").hide(200);
+    });
+
+        $("#btn-register").click(function(){
+            $(".main-bg-form-register").show(200);
+        });
+            $("#close-register").click(function(){
+                $(".main-bg-form-register").hide(200);
+            });
+
+                $("#btn-login").click(function(){
+                    $(".main-bg-form-register").hide(200);
+                });
+                    $("#btn-register").click(function(){
+                        $(".main-bg-form-login").hide(200);
+                    });
